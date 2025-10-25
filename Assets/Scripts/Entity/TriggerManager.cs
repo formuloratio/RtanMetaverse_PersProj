76dd0145008da599dcba211e304057742ec7773e
formuloratio-bit <formuloratio@gmail.com>
2025-10-25 23:19:35 +0900

    feat: 카메라 따라오기, 미니게임 트리거 연결
    트리거 -> 미니게임 전환시 디졸브 연출 포함

---
+++ Assets/Scripts/Entity/TriggerManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
+
+public class TriggerManager : MonoBehaviour
+{
+    public GameObject miniGameWindow;
+    public GameObject miniGameTrigger;
+    public GameObject loadingEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trigger")
+        {
+            StartCoroutine(MiniGameTriggerWait());
+        }
+    }
+
+    public void OnMiniGameYes()
+    {
+        Time.timeScale = 1;
+        OnLoadingEffect();
+        StartCoroutine(OnLoadingScene());
+    }
+
+    public void OnMiniGameNo()
+    {
+        miniGameWindow.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    IEnumerator MiniGameTriggerWait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Time.timeScale = 0;
+        miniGameWindow.SetActive(true);
+    }
+
+    IEnumerator OnLoadingScene()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("MiniGameScene");
+    }
+
+    private void OnLoadingEffect()
+    {
+        loadingEffect.SetActive(true);
+    }
+}
